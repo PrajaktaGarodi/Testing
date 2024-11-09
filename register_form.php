3398201bf27ec1ff8c9afa9dfbec727531989618
PrajaktaGarodi <prajaktagarodi@gmail.com>
2024-11-09 19:05:14 +0530

    html_validations

--- register_form.php
+++ register_form.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
+  <title>User Registration Form</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="jquery-ui.min.css">
@@ -26,13 +26,12 @@
   h6 {
     font-size: 20px;
     font-family: 'Times New Roman', Times, serif;
-
   }
 
   .error {
-  color: red;
-  font-style: italic;
-}
+    color: red;
+    font-style: italic;
+  }
 </style>
 
 <body>
@@ -47,98 +46,80 @@
                   style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
               </div>
               <div class="col-xl-6 p-3">
-                <form action="" method="POST" id="register">
+                <form action="register_form.php" method="POST" id="register">
                   <div class="card-body p-md-5 text-black">
                     <h3 class="mb-5 text-uppercase">User Registration form</h3>
 
                     <div class="row">
                       <div class="col-md-12 mb-4">
                         <div data-mdb-input-init class="form-outline">
-                          <label class="form-label" for="form3Example1m">User Name </label>
-                          <input type="text" id="username" class="form-control form-control-lg" name="username" />
+                          <label class="form-label" for="username">User Name </label>
+                          <input type="text" id="username" class="form-control form-control-lg" name="username" required />
                         </div>
                       </div>
                       <div class="col-md-12 mb-4">
                         <div data-mdb-input-init class="form-outline">
-                          <label class="form-label" for="form3Example1n">Email</label>
-                          <input type="email" id="email" class="form-control form-control-lg" name="email" />
+                          <label class="form-label" for="email">Email</label>
+                          <input type="email" id="email" class="form-control form-control-lg" name="email" required />
                         </div>
                       </div>
                     </div>
 
-
                     <div data-mdb-input-init class="form-outline mb-4">
-                      <label class="form-label" for="form3Example9">Contact</label>
-                      <input type="number" id="contact" class="form-control form-control-lg" name="contact" />
+                      <label class="form-label" for="contact">Contact</label>
+                      <input type="tel" id="contact" class="form-control form-control-lg" name="contact" required />
                     </div>
 
-
-                    <label class="form-label" for="form3Example8">Address</label>
+                    <label class="form-label" for="address">Address</label>
                     <div data-mdb-input-init class="form-outline mb-4">
-                      <input type="text" id="address" class="form-control form-control-lg" name="address" />
+                      <input type="text" id="address" class="form-control form-control-lg" name="address" required />
                     </div>
-
-
 
                     <div class="d-md-flex justify-content-start align-items-center mb-4 py-2">
-
-                      <h6 class="mb-0 me-4"> Gender : </h6>
+                      <h6 class="mb-0 me-4"> Gender: </h6>
 
                       <div class="form-check form-check-inline mb-0 me-4">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                          value="female" />
+                        <input class="form-check-input" type="radio" name="gender" id="femaleGender" value="female" required />
                         <label class="form-check-label" for="femaleGender">Female</label>
                       </div>
 
                       <div class="form-check form-check-inline mb-0 me-4">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
-                          value="male" />
+                        <input class="form-check-input" type="radio" name="gender" id="maleGender" value="male" />
                         <label class="form-check-label" for="maleGender">Male</label>
                       </div>
 
                       <div class="form-check form-check-inline mb-0">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
-                          value="other" />
+                        <input class="form-check-input" type="radio" name="gender" id="otherGender" value="other" />
                         <label class="form-check-label" for="otherGender">Other</label>
                       </div>
-
                     </div>
-
-
-
-
-
-
 
                     <div class="row">
                       <div class="col-md-12 mb-4">
                         <div data-mdb-input-init class="form-outline">
-                          <label class="form-label" for="form3Example1m">Password </label>
-                          <input type="text" id="password" name="password" class="form-control form-control-lg" />
+                          <label class="form-label" for="password">Password </label>
+                          <input type="password" id="password" name="password" class="form-control form-control-lg" required />
                         </div>
                       </div>
                       <div class="col-md-12 mb-4">
                         <div data-mdb-input-init class="form-outline">
-                          <label class="form-label" for="form3Example1n">Confirm Password</label>
-                          <input type="text" id="confirmpassword" name="confirmpassword"
-                            class="form-control form-control-lg" />
+                          <label class="form-label" for="confirmpassword">Confirm Password</label>
+                          <input type="password" id="confirmpassword" name="confirmpassword" class="form-control form-control-lg" required />
                         </div>
                       </div>
                     </div>
 
                     <div class="col-md-12 mb-4">
                       <div data-mdb-input-init class="form-outline">
-                        <label class="form-label" for="form3Example1n">Profile Photo</label>
-                        <input type="file" id="form3Example1n" class="form-control form-control-lg" />
+                        <label class="form-label" for="profilephoto">Profile Photo</label>
+                        <input type="file" id="profilephoto" name="profilephoto" class="form-control form-control-lg" />
                       </div>
                     </div>
                   </div>
 
-
                   <div class="d-flex justify-content-evenly pt-3">
                     <button type="reset" class="btn btn-light btn-lg">Reset All</button>
-                    <button type="submit" name="submit" class="btn btn-outline-secondary btn-lg ms-2">Submit
-                      Form</button>
+                    <button type="submit" name="submit" class="btn btn-outline-secondary btn-lg ms-2">Submit Form</button>
                   </div>
                 </form>
               </div>
@@ -147,11 +128,8 @@
         </div>
       </div>
     </div>
-    </div>
   </section>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
   <script src="js/jquery-3.7.1.min.js"></script>
   <script src="js/jquery.validate.min.js"></script>
   <script src="js/jquery-ui.min.js"></script>
